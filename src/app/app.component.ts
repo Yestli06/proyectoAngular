218b831398433fe91113bd35cc248e788e5e4627
SENA <SENA@BOGSIS04198>
2022-10-21 16:15:27 -0500

    calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent {
 
   pantalla1 = "";
   pantalla2 = "";
+  operador = "";
 
   num1(){
     this.pantalla1 = this.pantalla1 + "1";
@@ -51,6 +52,60 @@ export class AppComponent {
 
   num0(){
     this.pantalla1 = this.pantalla1 + "0";
+  }
+
+  borrar(){
+    this.pantalla1="";
+    this.pantalla2="";
+  }
+
+  borrar1(){
+    this.pantalla1="";
+ 
+    
+  }
+  
+  suma(){
+    this.pantalla2=this.pantalla1;
+    this.pantalla1="";
+    this.operador="suma";
+  }
+  
+  resta(){
+    this.pantalla2=this.pantalla1;
+    this.pantalla1="";
+    this.operador="resta";
+  }
+  
+  multiplica(){
+    this.pantalla2=this.pantalla1;
+    this.pantalla1="";
+    this.operador="multiplica";
+  }
+  
+  divide(){
+    this.pantalla2=this.pantalla1;
+    this.pantalla1="";
+    this.operador="divide";
+  }
+  
+  resultado(){
+    let valor1=Number(this.pantalla1);
+    let valor2=Number(this.pantalla2);
+  
+    if(this.operador =="suma"){
+      let suma = Number(valor1) + Number(valor2);
+      this.pantalla1 = suma.toString();
+    }else if(this.operador=="resta"){
+      let suma = Number(valor2) - Number(valor1);
+      this.pantalla1 = suma.toString();
+    }else if(this.operador=="multiplica"){
+      let suma = Number(valor1) * Number(valor2);
+      this.pantalla1 = suma.toString();
+    }else if(this.operador=="divide"){
+      let suma = Number(valor2) / Number(valor1);
+      this.pantalla1 = suma.toString();
+    }
   }
 
   
